bb88e824967f6051c8829f6e5ee35dcc23cd10e0
Ludwig.Stanley <Ludwig.stnl.prz@gmail.com>
2023-09-01 08:08:24 -0600

    captura de datos

--- ejercicio1-dps/src/App.js
+++ ejercicio1-dps/src/App.js
@@ -1,84 +1,12 @@
 import React, { useState } from 'react';
 import './FormularioCursos.css';
+import './components/Cliente';
+import Formulario from './components/Cliente';
 
-function FormularioCursos() {
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [curso, setCurso] = useState('ingles');
-  const [cantidadMeses, setCantidadMeses] = useState(1);
-
-  const handleNombreChange = (event) => {
-    setNombre(event.target.value);
-  };
-
-  const handleApellidoChange = (event) => {
-    setApellido(event.target.value);
-  };
-
-  const handleCursoChange = (event) => {
-    setCurso(event.target.value);
-  };
-
-  const handleCantidadMesesChange = (event) => {
-    setCantidadMeses(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Aquí puedes realizar cualquier acción con los datos capturados, como enviarlos a un servidor o mostrarlos en la consola.
-    console.log('Nombre:', nombre);
-    console.log('Apellido:', apellido);
-    console.log('Curso:', curso);
-    console.log('Cantidad de Meses:', cantidadMeses);
-  };
-
-  return (
-    <div className="formulario-container">
-      <h2>Formulario de Inscripción a Cursos</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="nombre">Nombre:</label>
-          <input
-            type="text"
-            id="nombre"
-            value={nombre}
-            onChange={handleNombreChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="apellido">Apellido:</label>
-          <input
-            type="text"
-            id="apellido"
-            value={apellido}
-            onChange={handleApellidoChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="curso">Curso:</label>
-          <select id="curso" value={curso} onChange={handleCursoChange}>
-            <option value="ingles">Inglés</option>
-            <option value="frances">Francés</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="cantidadMeses">Cantidad de Meses a Pagar:</label>
-          <input
-            type="number"
-            id="cantidadMeses"
-            value={cantidadMeses}
-            onChange={handleCantidadMesesChange}
-            min="1"
-            required
-          />
-        </div>
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
-  );
+function PaginaPrincipal() {
+return(
+  <Formulario/>
+);
 }
 
-export default FormularioCursos
+export default PaginaPrincipal;
--- ejercicio1-dps/src/components/Cliente.jsx
+++ ejercicio1-dps/src/components/Cliente.jsx
@@ -1,5 +1,82 @@
 import React,{useState} from "react";
+import '../FormularioCursos.css';
 
-const Cliente=()=>{
-    
+const Formulario=()=>{
+    const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [curso, setCurso] = useState('ingles');
+  const [cantidadMeses, setCantidadMeses] = useState(1);
+
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
+  };
+
+  const handleApellidoChange = (event) => {
+    setApellido(event.target.value);
+  };
+
+  const handleCursoChange = (event) => {
+    setCurso(event.target.value);
+  };
+
+  const handleCantidadMesesChange = (event) => {
+    setCantidadMeses(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('Nombre:', nombre);
+    console.log('Apellido:', apellido);
+    console.log('Curso:', curso);
+    console.log('Cantidad de Meses:', cantidadMeses);
+  };
+  return (
+    <div className="formulario-container">
+      <h2>Formulario de Inscripción a Cursos de Inglés y Frances</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="nombre">Nombre:</label>
+          <input
+            type="text"
+            id="nombre"
+            value={nombre}
+            onChange={handleNombreChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="apellido">Apellido:</label>
+          <input
+            type="text"
+            id="apellido"
+            value={apellido}
+            onChange={handleApellidoChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="curso">Curso:</label>
+          <select id="curso" value={curso} onChange={handleCursoChange}>
+            <option value="ingles">Inglés</option>
+            <option value="frances">Francés</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="cantidadMeses">Cantidad de Meses a Pagar:</label>
+          <input
+            type="number"
+            id="cantidadMeses"
+            value={cantidadMeses}
+            onChange={handleCantidadMesesChange}
+            min="1"
+            required
+          />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
+
+export default Formulario;
