e2d42852762762644e44a9e82deacd795cd917fe
LuisDC2001 <luisdelcid248@gmail.com>
2023-09-01 08:54:31 -0600

    Agregando Funciones

--- ejercicio1-dps/src/components/Cliente.jsx
+++ ejercicio1-dps/src/components/Cliente.jsx
@@ -2,10 +2,11 @@ import React,{useState} from "react";
 import '../FormularioCursos.css';
 
 const Formulario=()=>{
-    const [nombre, setNombre] = useState('');
+  const [nombre, setNombre] = useState('');
   const [apellido, setApellido] = useState('');
   const [curso, setCurso] = useState('ingles');
   const [cantidadMeses, setCantidadMeses] = useState(1);
+ 
 
   const handleNombreChange = (event) => {
     setNombre(event.target.value);
@@ -31,6 +32,32 @@ const Formulario=()=>{
     console.log('Curso:', curso);
     console.log('Cantidad de Meses:', cantidadMeses);
   };
+
+  const precioPorMes = 25;
+  let descuento = 0;
+  let porcentajeDescuento = 0;
+
+  if (cantidadMeses >= 2 && cantidadMeses <= 3) {
+    descuento = precioPorMes * cantidadMeses * 0.1;
+    porcentajeDescuento = 10;
+  } else if (cantidadMeses >= 4 && cantidadMeses <= 5) {
+    descuento = precioPorMes * cantidadMeses * 0.2;
+    porcentajeDescuento = 20;
+  } else if (cantidadMeses >= 6 && cantidadMeses <= 7) {
+    descuento = precioPorMes * cantidadMeses * 0.27;
+    porcentajeDescuento = 27;
+  } else if (cantidadMeses >= 8 && cantidadMeses <= 9) {
+    descuento = precioPorMes * cantidadMeses * 0.37;
+    porcentajeDescuento = 37;
+  } else if (cantidadMeses >= 10) {
+    descuento = precioPorMes * cantidadMeses * 0.53;
+    porcentajeDescuento = 53;
+  }
+
+  const totalAPagar = precioPorMes * cantidadMeses - descuento;
+  
+
+
   return (
     <div className="formulario-container">
       <h2>Formulario de Inscripción a Cursos de Inglés y Frances</h2>
@@ -74,6 +101,18 @@ const Formulario=()=>{
           />
         </div>
         <button type="submit">Enviar</button>
+
+        <div>
+      <h2>Resumen de la Inscripción</h2>
+      <p>Nombre del Cliente: {nombre}</p>
+      <p>Apellido del Cliente: {apellido}</p>
+      <p>Tipo de Curso: {curso}</p>
+      <p>Cantidad de Meses a Pagar: {cantidadMeses}</p>
+      <p>Descuento Calculado: ${descuento.toFixed(2)}</p>
+      <p>Porcentaje de Descuento: {porcentajeDescuento}%</p>
+      <p>Total a Pagar: ${totalAPagar.toFixed(2)}</p>
+    </div>
+
       </form>
     </div>
   );
